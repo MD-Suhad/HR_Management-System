8e242182f38feb975b11ab0b1e37b4e10288bb3a
MD-Suhad <ssuhad490@gmail.com>
2024-05-08 22:41:54 +0600

    database added

--- src/main/java/com/alibou/security/user/User.java
+++ src/main/java/com/alibou/security/user/User.java
@@ -18,18 +18,7 @@ import org.springframework.security.core.token.Token;
 import org.springframework.security.core.userdetails.UserDetails;
 
 
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//
-//import java.util.Collection;
-//import java.util.Date;
-//import java.util.List;
+
 
 @Data
 @Builder
