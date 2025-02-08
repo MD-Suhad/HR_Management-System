441535efe0ba3042587c0f9c3df568351e450564
MD-Suhad <ssuhad490@gmail.com>
2025-02-08 23:47:24 +0600

    added entity

---
+++ src/main/java/com/alibou/security/entity/Country.java
@@ -0,0 +1,13 @@
+package com.alibou.security.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+public class Country {
+    private int countryId;
+
+    private String name;
+
+    private String continent;
+}
