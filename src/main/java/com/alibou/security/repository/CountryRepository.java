45e5e14e4deffead9684c1039b6226f95fd84a27
MD-Suhad <ssuhad490@gmail.com>
2025-02-25 23:05:13 +0600

    updated code

--- src/main/java/com/alibou/security/repository/CountryRepository.java
+++ src/main/java/com/alibou/security/repository/CountryRepository.java
@@ -1,8 +1,12 @@
 package com.alibou.security.repository;
 
 
+import com.alibou.security.entity.Country;
+import jakarta.persistence.Id;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CountryRepository {
+public interface CountryRepository extends CrudRepository<Country, Id> {
+
 }
