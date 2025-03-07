3ffcc0c8a2b492c7cbd4225433c84b46fd18f1d2
MD-Suhad <ssuhad490@gmail.com>
2025-03-07 23:30:24 +0600

    constant package added

---
+++ src/main/java/com/alibou/security/constant/AppConstant.java
@@ -0,0 +1,9 @@
+package com.alibou.security.constant;
+
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class AppConstant {
+}
--- src/main/java/com/alibou/security/entity/Country.java
+++ src/main/java/com/alibou/security/entity/Country.java
@@ -1,10 +1,11 @@
 package com.alibou.security.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
+@Data
 @Setter
 @JsonIgnoreProperties
 
