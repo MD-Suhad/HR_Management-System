a7e1882f62c535169d89efe527e3fa4fd1ea6a72
MD-Suhad <ssuhad490@gmail.com>
2025-09-20 00:40:45 +0600

    project structure updated

--- src/main/java/com/pos/shohaib/security/controller/UserController.java
+++ src/main/java/com/pos/shohaib/security/controller/UserController.java
@@ -6,15 +6,19 @@ import com.pos.shohaib.security.service.User.UserService;
 import com.pos.shohaib.security.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
 @Validated
+@RequestMapping("/api/v1/auth")
+@Controller
 public class UserController {
     @Autowired
     private UserService userService;
